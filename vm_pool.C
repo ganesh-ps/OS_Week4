9d44b561ba542454860c4d91d3ff58a6e93c1f2c
sath_ganp <sath_ganp@tamu.edu>
2019-03-20 16:48:16 -0500

    directory from process pool

--- vm_pool.C
+++ vm_pool.C
@@ -68,64 +68,64 @@ VMPool::VMPool(unsigned long  _base_address,
 
 unsigned long VMPool::allocate(unsigned long _size) {
     if(_size>free_size){
-	assert(false);
-    }
+		assert(false);
+	}
 	
-    unsigned long base_address_allocated = base_address + (pool_size-free_size);
+	unsigned long base_address_allocated = base_address + (pool_size-free_size);
 	
-    unsigned long pages_to_be_allocated;
+	unsigned long pages_to_be_allocated;
     if((_size%4096)!=0){
-	pages_to_be_allocated = _size/4096 + 1;
-    }
-    else{
-	pages_to_be_allocated = _size/4096;
-    }
+		pages_to_be_allocated = _size/4096 + 1;
+	}
+	else{
+		pages_to_be_allocated = _size/4096;
+	}
 
-    allocated_regions[nregions_allocated].base_address = base_address_allocated;
-    allocated_regions[nregions_allocated].size =pages_to_be_allocated*4096;
+	allocated_regions[nregions_allocated].base_address = base_address_allocated;
+	allocated_regions[nregions_allocated].size =pages_to_be_allocated*4096;
 	
-    free_size = free_size - (pages_to_be_allocated*4096);
+	free_size = free_size - (pages_to_be_allocated*4096);
 	
-    nregions_allocated++;
+	nregions_allocated++;
 	
-    Console::puts("Allocated region of memory.\n");
-    //Console::puts("allocated address");Console::putui((unsigned int)pages_to_be_allocated);Console::puts("\n");
-    return (base_address_allocated);
+	Console::puts("Allocated region of memory.\n");
+	//Console::puts("allocated address");Console::putui((unsigned int)pages_to_be_allocated);Console::puts("\n");
+	return (base_address_allocated);
 }
 
 void VMPool::release(unsigned long _start_address) {
     //assert(false);
 	
-    unsigned int index_of_removed=-1;
+	unsigned int index_of_removed=-1;
     unsigned long size_of_released=0;
     unsigned long npages_released;	
-    unsigned long start_address = _start_address;
-    //Console::puts("Start address");Console::putui((unsigned int)start_address);Console::puts("\n");
-    for(int i=0;i<nregions_allocated;i++)
-    {
-	//Console::puts("list");Console::putui((unsigned int)allocated_regions[i].base_address);Console::puts("\n");
-	if(allocated_regions[i].base_address == start_address){
-	index_of_removed = i;
-	size_of_released = allocated_regions[i].size;
-	//Console::puts("Size_released");Console::putui((unsigned int)allocated_regions[i].size);Console::puts("\n");
-	break;
+	unsigned long start_address = _start_address;
+	//Console::puts("Start address");Console::putui((unsigned int)start_address);Console::puts("\n");
+	for(int i=0;i<nregions_allocated;i++)
+	{
+		//Console::puts("list");Console::putui((unsigned int)allocated_regions[i].base_address);Console::puts("\n");
+		if(allocated_regions[i].base_address == start_address){
+			index_of_removed = i;
+			size_of_released = allocated_regions[i].size;
+			//Console::puts("Size_released");Console::putui((unsigned int)allocated_regions[i].size);Console::puts("\n");
+			break;
+		}
+	}		
+	assert(index_of_removed!=-1);
+	for(int i =index_of_removed+1;i<nregions_allocated;i++){
+		allocated_regions[i-1] = allocated_regions[i];
 	}
-    }		
-    assert(index_of_removed!=-1);
-    for(int i =index_of_removed+1;i<nregions_allocated;i++){
-	allocated_regions[i-1] = allocated_regions[i];
-    }
 
-    nregions_allocated--;
-    free_size = free_size+size_of_released;
+	nregions_allocated--;
+	free_size = free_size+size_of_released;
 
-    //Call free_page
-    npages_released = size_of_released/4096;
-    //Console::puts("pages_released");Console::putui((unsigned int)npages_released);Console::puts("\n");		
-    for(int i=0; i<npages_released;i++){
-	page_table->free_page(start_address);
-	start_address += 4096;
-    }
+	//Call free_page
+	npages_released = size_of_released/4096;
+	//Console::puts("pages_released");Console::putui((unsigned int)npages_released);Console::puts("\n");		
+	for(int i=0; i<npages_released;i++){
+		page_table->free_page(start_address);
+		start_address += 4096;
+	}
 	
     Console::puts("Released region of memory.\n");
 }
@@ -144,3 +144,31 @@ bool VMPool::is_legitimate(unsigned long _address) {
 	
     Console::puts("Checked whether address is part of an allocated region.\n");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
